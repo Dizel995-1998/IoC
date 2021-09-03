6363a44aef223155ccfb26d8d6946cd85fcea9f3
Шамиль Прилов <pri@crtweb.ru>
2021-09-03 12:39:16 +0300

    added-container-interface

---
+++ src/Container/ContainerInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Creative\IoC\Container;
+
+use Creative\IoC\ServiceResolver;
+
+interface ContainerInterface
+{
+    public function get(string $id) : ?ServiceResolver;
+
+    /**
+     * Returns true if the container can return an entry for the given identifier.
+     * Returns false otherwise.
+     *
+     * `has($id)` returning true does not mean that `get($id)` will not throw an exception.
+     * It does however mean that `get($id)` will not throw a `NotFoundExceptionInterface`.
+     *
+     * @param string $id Identifier of the entry to look for.
+     *
+     * @return bool
+     */
+    public function has(string $id): bool;
+}
